63771cda95922acb4f6f2310057480e45f19a432
Serianu Andrei-Silviu <andreisilviu.serianu@student.griffith.ie>
2025-02-19 11:45:46 +0000

    Task 1 Part 2

--- src/griffith/GradesTest.java
+++ src/griffith/GradesTest.java
@@ -11,21 +11,37 @@ import org.junit.jupiter.api.Test;
 public class GradesTest {
     @Test
     public void testGradesMax() {
-        // Placeholder test
+        Grades g = new Grades();
+        int[] grades = {45, 78, 90, 32, 88};
+        assertEquals(90, g.gradesMax(grades));
+
+        int[] negativeGrades = {-10, -5, -20, -3};
+        assertEquals(-3, g.gradesMax(negativeGrades));
+
+        int[] singleElement = {50};
+        assertEquals(50, g.gradesMax(singleElement));
     }
 
     @Test
     public void testGradesTotal() {
-        // Placeholder test
+        Grades g = new Grades();
+        int[] grades = {10, 20, 30, 40};
+        assertEquals(100, g.gradesTotal(grades));
+
+        int[] emptyArray = {};
+        assertEquals(0, g.gradesTotal(emptyArray));
     }
 
     @Test
     public void testGradesAverage() {
-        // Placeholder test
+        Grades g = new Grades();
+        int[] grades = {50, 60, 70, 80};
+        assertEquals(65.0, g.gradesAverage(grades), 0.01);
     }
 
     @Test
     public void testCountFails() {
-        // Placeholder test
-}
+        Grades g = new Grades();
+        int[] grades = {10, 20, 50, 70};
+        assertEquals(2, g.countFails(grades,40));;}
 }
