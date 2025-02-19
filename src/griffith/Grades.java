91c77580e034aa4819b329c0df6e05263d3fc2d4
Serianu Andrei-Silviu <andreisilviu.serianu@student.griffith.ie>
2025-02-19 11:51:13 +0000

    Task 1 Part 3

--- src/griffith/Grades.java
+++ src/griffith/Grades.java
@@ -1,20 +1,21 @@
 package griffith;
+import java.util.*;
 
 public class Grades {
-
     public int gradesMax(int[] grades) {
-        return 0; 
+        if (grades.length == 0) return Integer.MIN_VALUE;
+        return Arrays.stream(grades).max().getAsInt();
     }
 
     public int gradesTotal(int[] grades) {
-        return 0; 
+        return Arrays.stream(grades).sum();
     }
 
     public double gradesAverage(int[] grades) {
-        return 0.0; 
+        if (grades.length == 0) return 0.0;
+        return (double) gradesTotal(grades) / grades.length;
     }
 
     public int countFails(int[] grades, int minGrade) {
-        return 0;
-    }
+        return (int) Arrays.stream(grades).filter(g -> g < minGrade).count();}
 }
