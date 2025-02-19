83537f2ce12aa3404be9b9e402328e35f02c5970
Serianu Andrei-Silviu <andreisilviu.serianu@student.griffith.ie>
2025-02-19 11:58:45 +0000

    Task 2 Part 1

---
+++ src/griffith/Word.java
@@ -0,0 +1,20 @@
+package griffith;
+
+public class Word {
+    private char[] letters;
+
+    public Word(char[] letters) {
+        this.letters = letters;
+    }
+
+    public boolean contains(char symbol) {
+        return false;
+    }
+
+    public int length() {
+        return 0;
+    }
+
+    public char[] getLetters() {
+        return null;}
+}
---
+++ src/griffith/WordTest.java
@@ -0,0 +1,21 @@
+package griffith;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class WordTest {
+    @Test
+    public void testContains() {
+        
+    }
+
+    @Test
+    public void testLength() {
+       
+    }
+
+    @Test
+    public void testNotNull() {
+
+}
+}
